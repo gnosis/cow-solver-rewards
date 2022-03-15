519f567cf6c982280bf4073c848f00f80c1617e8
josojo <josojo@hotmail.de>
2022-03-15 13:56:16 +0100

    update on the test description

--- tests/e2e/test_internal_trades.py
+++ tests/e2e/test_internal_trades.py
@@ -114,8 +114,9 @@ class TestDuneAnalytics(unittest.TestCase):
     def test_does_recognize_slippage_due_to_buffer_token_list(self):
         """
         tx: 0x0bd527494e8efbf4c3013d1e355976ed90fa4e3b79d1f2c2a2690b02baae4abe
-        This tx has a internal trade between pickle and eth. As pickle is not in the allow-list, hence 
-        the internal trade was not allowed, our queries should recognized it as slippage as not as a internal trade. 
+        This tx has a internal trade between pickle and eth. As pickle is not in the allow-list, 
+        the internal trade was not allowed.
+        Our queries should recognized these kind of trades as slippage and not as a internal trades. 
         """
         internal_transfers = get_internal_transfers(
             dune=self.dune_connection,
