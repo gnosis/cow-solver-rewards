63a0215c41340b7debcd3365bb33b400a3f75c80
Ben Smith <bh2smith@gmail.com>
2022-03-17 19:50:18 +0100

    unused import

--- tests/e2e/test_internal_trades.py
+++ tests/e2e/test_internal_trades.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import unittest
 from dataclasses import dataclass
-from datetime import datetime
 from enum import Enum
 
 from src.dune_analytics import DuneAnalytics, QueryParameter
